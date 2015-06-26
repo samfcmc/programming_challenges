f1c96d1a64c9e214d4fc47e8c9a315a5495e8a93
Samuel Coelho <samuelfcmc@gmail.com>
2015-06-26 16:23:08 +0100

    Implemented the first exercise

--- cracking_coding_interview/c1_arrays_and_strings/ex1_string_unique_chars.c
+++ cracking_coding_interview/c1_arrays_and_strings/ex1_string_unique_chars.c
@@ -3,7 +3,57 @@
  * Determine if a string has all unique characters
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * The latin alphabet has 26 letters but
+ * we need to multiply by 2 to have in
+ * account that each letter can be
+ * upper or lower case
+ */
+#define LETTERS 'z' - 'A'
+
+#define FIRST_ASCII_CODE 'A'
+
+int has_unique_chars(char *string) {
+
+  int count[LETTERS];
+
+  for (size_t i = 0; i < LETTERS; i++) {
+    count[i] = 0;
+  }
+
+  char *c = string;
+
+  while(*c) {
+    int index = *c - FIRST_ASCII_CODE;
+    int value = count[index];
+    if(value) {
+      return !0;
+    }
+    else {
+      count[index]++;
+      c++;
+    }
+  }
+
+  return 0;
+
+}
+
 int main() {
-  
+  char *line = NULL;
+  size_t len = 0;
+  size_t read;
+
+  while ((read = getline(&line, &len, stdin)) != -1) {
+    if(read > 0) {
+      // Remove last '\n'
+      line[read - 1] = '\0';
+      printf("%d\n", has_unique_chars(line));
+    }
+  }
+
   return 0;
 }
